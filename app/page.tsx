21db838ece1fef12a65e53223b4fdaeeea51cbbb
weird-samuel <fiyinfoluwasamuel580@gmail.com>
2024-07-20 08:25:54 +0100

    fix build error

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function LandingPage() {
   return (
     <main className="container xl:px-28">
       <h1>Nothing 2 Prove</h1>
-      <p>The official website of the "Nothing 2 Prove" streetwear brand.</p>
+      <p>The official website of the Nothing 2 Prove streetwear brand.</p>
     </main>
   );
 }
