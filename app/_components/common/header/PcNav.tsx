fc008d31982c6c6a53210275f575ff43c964a34e
weird-samuel <fiyinfoluwasamuel580@gmail.com>
2024-08-09 23:41:31 +0100

    project setup with database connected

--- app/_components/common/header/PcNav.tsx
+++ app/_components/common/header/PcNav.tsx
@@ -24,7 +24,18 @@ export const PcNav = () => {
     <nav className="w-full flex items-center justify-between py-6 xl:px-20">
       {/* Left Section */}
       <div className="flex items-center space-x-28">
-        <div className="text-gray-800 text-2xl font-bold">N2P</div>
+        <Link href={"/"}>
+          <Image
+            src="/images/logo.png"
+            alt="Logo"
+            width={80}
+            height={50}
+            style={{
+              width: "80px",
+              height: "50px",
+            }}
+          />
+        </Link>
         <div className="hidden md:flex space-x-6">
           {leftSection.map((item, index) => (
             <Link
@@ -52,7 +63,16 @@ export const PcNav = () => {
             className="text-gray-900 space-y-1 text-[16px] tracking-wide"
           >
             <div className="flex justify-center items-center">
-              <Image src={item.icon} alt={item.label} width={20} height={20} />
+              <Image
+                src={item.icon}
+                alt={item.label}
+                width={20}
+                height={20}
+                style={{
+                  width: "20px",
+                  height: "20px",
+                }}
+              />
             </div>
             <p className="font-semibold ">{item.label}</p>
           </Link>
