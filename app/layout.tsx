4665bfafb159bbe4278d75e7d36bd53e7ec4bcef
try-samuel <fiyinfoluwasamuel580@gmail.com>
2025-01-20 17:47:01 +0100

    trying something

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
     url: "https://www.nth2prove.shop",
     images: [
       {
-        url: "https://www.nth2prove.shop/images/logo.png",
+        url: "https://instagram.flos2-2.fna.fbcdn.net/v/t51.29350-15/437589249_798363092341272_989318966013956397_n.jpg?stp=dst-jpg_e35_p1080x1080_tt6&efg=eyJ2ZW5jb2RlX3RhZyI6ImltYWdlX3VybGdlbi4xNDQweDE4MDAuc2RyLmYyOTM1MC5kZWZhdWx0X2ltYWdlIn0&_nc_ht=instagram.flos2-2.fna.fbcdn.net&_nc_cat=100&_nc_ohc=NzDrchhfjyIQ7kNvgHTLD8G&_nc_gid=3ce1c4d82bf44e04b97b17e3531aa169&edm=AP4sbd4BAAAA&ccb=7-5&ig_cache_key=MzM0NDc0NjM2NTY4Njc2NzU1Mg%3D%3D.3-ccb7-5&oh=00_AYAWPn4-89jrk16ew_bCRkZT-S9yLE8r8mWdG8mTdwEGxA&oe=67943EFA&_nc_sid=7a9f4b",
         width: 800,
         height: 600,
         alt: "N2P Brand Logo",
@@ -34,7 +34,8 @@ export const metadata: Metadata = {
     title: "N2P | Nothing To Prove",
     description:
       "A streetwear brand that conveys the idea that true style and personal worth do not need validation from others.",
-    images: "https://www.nth2prove.shop/images/logo.png",
+    images:
+      "https://instagram.flos2-2.fna.fbcdn.net/v/t51.29350-15/437589249_798363092341272_989318966013956397_n.jpg?stp=dst-jpg_e35_p1080x1080_tt6&efg=eyJ2ZW5jb2RlX3RhZyI6ImltYWdlX3VybGdlbi4xNDQweDE4MDAuc2RyLmYyOTM1MC5kZWZhdWx0X2ltYWdlIn0&_nc_ht=instagram.flos2-2.fna.fbcdn.net&_nc_cat=100&_nc_ohc=NzDrchhfjyIQ7kNvgHTLD8G&_nc_gid=3ce1c4d82bf44e04b97b17e3531aa169&edm=AP4sbd4BAAAA&ccb=7-5&ig_cache_key=MzM0NDc0NjM2NTY4Njc2NzU1Mg%3D%3D.3-ccb7-5&oh=00_AYAWPn4-89jrk16ew_bCRkZT-S9yLE8r8mWdG8mTdwEGxA&oe=67943EFA&_nc_sid=7a9f4b",
   },
 };
 
@@ -48,7 +49,7 @@ export default function RootLayout({
     "@type": "Brand",
     name: "N2P",
     url: "https://www.nth2prove.shop",
-    logo: "https://www.nth2prove.shop/images/logo.png",
+    logo: "https://instagram.flos2-2.fna.fbcdn.net/v/t51.29350-15/437589249_798363092341272_989318966013956397_n.jpg?stp=dst-jpg_e35_p1080x1080_tt6&efg=eyJ2ZW5jb2RlX3RhZyI6ImltYWdlX3VybGdlbi4xNDQweDE4MDAuc2RyLmYyOTM1MC5kZWZhdWx0X2ltYWdlIn0&_nc_ht=instagram.flos2-2.fna.fbcdn.net&_nc_cat=100&_nc_ohc=NzDrchhfjyIQ7kNvgHTLD8G&_nc_gid=3ce1c4d82bf44e04b97b17e3531aa169&edm=AP4sbd4BAAAA&ccb=7-5&ig_cache_key=MzM0NDc0NjM2NTY4Njc2NzU1Mg%3D%3D.3-ccb7-5&oh=00_AYAWPn4-89jrk16ew_bCRkZT-S9yLE8r8mWdG8mTdwEGxA&oe=67943EFA&_nc_sid=7a9f4b",
     sameAs: [
       "https://twitter.com/yourTwitterHandle",
       "https://www.instagram.com/yourInstagramHandle",
